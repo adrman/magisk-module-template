6b0ec855d0b6f654cd8b7d4d9e160019186408dd
Bill <adrman@users.noreply.github.com>
2016-10-30 10:57:25 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -36,7 +36,7 @@ CACHEMOD=false
 
 # This will be the folder name under /magisk or /cache/magisk
 # This should also be the same as the id in your module.prop to prevent confusion
-MODID=GSAM-PRO
+MODID=ACTION_LAUNCHER_GSAM-PRO
 
 # Set to true if you need automount
 # Most mods would like it to be enabled
